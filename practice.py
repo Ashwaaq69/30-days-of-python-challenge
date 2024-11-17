84e6f38d184d836f74ca9e993b351c247879a25d
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2024-11-17 22:17:16 +0300

    Add Day 1 challenges code

---
+++ practice.py
@@ -0,0 +1,103 @@
+# some of built in functions 
+
+''' 
+type chencks the data type
+print(type(6))
+print(type('heeloo'));
+input("Enter your name");
+print (sum([2,3,4]));
+
+'''
+
+# variables
+# first_name= "Ashuu";
+# last_name= "mohaz";
+# country= "somalia"
+# age= "22";
+# is_maried= False
+
+# print('first_Name is :' + len(first_name));
+# print('full_name is :'+ first_name +'' + last_name );
+# print("age is : " + age)
+
+# skills = ['htmls', 'css', 'js', 'react', 'python']
+
+# personal_info = {
+#     "first_name": "Ashuu",
+#     "last_name" : "mohaz",
+#     "country" : "somalia",
+#     "age" : 22
+
+# }
+
+# print(first_name);
+# print(personal_info)
+# print(len('ashuu mohaz!'));
+#using str.format()
+
+# first_name = personal_info["first_name"]
+# last_name = personal_info["last_name"]
+# age = personal_info["age"]
+# country = personal_info["country"]
+
+# # Format the output using str.format()
+# output = "name: {} {}, age: {}, country: {}".format(first_name, last_name, age, country)
+
+# # Print the result
+# print(output)
+
+
+#Multiple variables can also be declared in one line:
+
+# first_name, last_name, country, is_maried, age = "Ashuu", "mohaz", "somalia", "no", 22
+
+# print(first_name, last_name, country, is_maried, age)
+
+#Getting user input using the input() built-in function. 
+
+# Name = input("what isnyour name:")
+# age = input("how old are you?")
+# city = input("which city you came from?")
+
+# print("ur name is :" + Name + "\n " "ur age is :" + age + "\n " "ur city is: " + city)
+# # print(age)
+# # print(city)
+
+
+#Data Types
+ # int, float, str, bool, list, tuple, set, dict
+# print(type(10)) #int
+# print(type(10.5)) #float
+# print(type('hello')) #str
+# print(type(22)) #int
+# print(type(22.5)) #float
+# print(type(True)) #bool
+# print(type([1,2,3])) #list
+# print(type(('apple', 'banana'))) #tuple
+# print(type({1:'apple', 2:'banana'})) #dict
+# print(type((5,8))) #tuple
+# print(type(zip([1,2],['ashuu'], [4,5]))) #zip object set
+
+
+#  Converting one data type to another data type
+#int to float
+# num_int = 44
+# print(num_int)
+# num_float = float (num_int)
+# print(num_float)
+
+# #float to int
+# gravity = 1.8
+# print(int(gravity))
+
+#string to int
+# string_int = "44"
+# print('num int ', int(string_int))
+ 
+#  #str to list
+ 
+string_list = list('hello')
+print(string_list)
+
+
+
