d0864097bb3f77815d886ef272f6f805ea7dad03
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2024-11-18 17:42:12 +0300

    Add day2practice.py and day2Exercise.py

---
+++ day2/day2Exercise.py
@@ -0,0 +1,65 @@
+# age = 22
+# height = 1.75
+# complex_num = (2+2j) * (1+1)
+# base =  float (input("enter base:"))
+# print(base)
+# height = float(input("enter height:"))
+# print(height)
+# area_of_triange = 0.5 * base * height
+# print('area of triangle is :', area_of_triange)
+
+# a = int(input('enter side a:'))
+
+
+# b = int(input('enter side b:'))
+
+# c =int(input('enter side c:'))
+
+# primeter = a + b + c
+# print('the primeter of the triangle is ', primeter)
+
+# 6.Get length and width of a rectangle using prompt.
+# length = int(input('enter length:'))
+# width = int(input('enter width:'))
+# perimeter = 2 * (length + width)
+# print('The perimeter of the rectangle is:', perimeter)
+
+#7 Get radius of a circle using prompt.
+
+# radius = float(input('enter radius:'))
+# area_of_circle = 3.14 * radius * radius
+
+# print('The area of the circle is:', area_of_circle)
+
+#8 Calculate the slope, x-intercept and y-intercept of y = 2x -2
+# slope = 2
+# x_intercept = -2/2
+# y_intercept = 2 * (-2)
+
+# print('slope is:', slope)
+
+# print('x-intercept is:', x_intercept)
+# print('y-intercept is:', y_intercept)
+
+#9Slope is (m = y2-y1/x2-x1).Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+
+# x1, y1 = 2,2
+# x2, y2 = 6,8
+# slope = (y2 - y1)/(x2 - x1)
+# distance = ((x2 - x1)**2 + (y2-y1)**2) **0.5
+# print("Slope:", slope)
+# print("Euclidean distance: ", distance)
+
+#10 compare the slopes in tasks 8 and 9
+slope_8 = 2
+slope_9 = (8-2)/(6-2)
+
+if slope_8 == slope_9:
+    print('Slopes are equal')
+else:
+    print('Slopes are not equal')
+
+
+
+
+
---
+++ day2/day2practice.py
@@ -0,0 +1,117 @@
+# day2 booleans
+# booleans are true or false
+
+# print(True)
+# print(False)
+
+#arthimatic operators in python
+#intigers
+# print('Aditions: ', 2+2)
+# print('subtraction: ', 10-2)
+# print('multiplication: ', 4*4)
+# print('Division: ', 6/2)  #division in python gives floating
+# print('Division without reminder:', 7//2)
+# print('modules:', 5%2)
+# print('exponentiation: ', 2**4)
+
+#float
+# print('float addition: ', 2.5+2.5)
+# print('float point, number, PI', 3.14)
+# print('float point, gravity', 9.81)
+
+#complex numbers
+
+# print('complex num:' , 1+1j)
+# print('multiplying complex nums', (1+1j) * (1-1j))
+
+#declare avariable
+# a = 4
+# b = 8
+# total = a+b
+# dif = a-b
+# product = a*b
+# division = a/b
+# reminder = a%b
+# floor_div = a//b
+# expo = a**b
+
+# print('Total:', total)
+
+# print('Difference:', dif)
+# print('Product:', product)
+
+
+# print('Division:', division)
+# print('Reminder:', reminder)
+
+# print('Floor Division:', floor_div)
+# print('Exponentiation:', expo)  
+
+#calculating area of circle
+# radius = 10
+# area_circle = 3.14 * radius**2
+# print('Area of circle: ', area_circle)
+
+
+# #calculating area of rectangle
+# length = 10
+# width = 20
+# area_rectangle = length * width
+# print('Area of rectangle: ', area_rectangle)
+
+# #calculating a weight of an object
+
+# mass = 5
+# gravity = 9.81
+# weight = mass * gravity
+# print(weight, 'N')
+
+# #calculate the density of a liquid
+# mass = 75 #in kg
+# volume = 0.075 #in cubic meter
+
+# density = mass / volume
+# print('the density of li is: ' , density)
+
+# #comparison operator
+# print(4>5)
+# print(4>=5)
+
+# print(4<5)
+# print(4<=5)
+
+# print(6==5)
+# print(6!=5)
+
+# print(len('mango') == len('lemon'))
+# print(len('mango') != len('lemon'))
+# print(len('mango') < len('lemon'))
+# print(len('mango') > len('lemon'))
+
+# Comparing something gives either a True or False
+# print('True == True: ', True == True)
+# print('True == False: ', True == False)
+# print('False == False:', False == False)
+
+# print('1 is 1', 1 is 1)
+# print('1 is not 1', 1 is not 2)
+# print('A in Ashuu', 'B' in 'Ashuu')
+# print('4 is 2**2:', 4 is 2**2)
+
+#logical operators
+print(5 > 3 and 3 > 1)
+print('true and true:', True and True )
+print(5 > 3 or 3 > 1)
+print(not 5 > 3)
+print(not True)
+print(not False)
+
+
+
+
+
+
+
+
+
+
